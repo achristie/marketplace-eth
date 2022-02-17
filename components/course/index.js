0030bbdc94a5b5f664ac52d48d9d835f35463f89
Andrew Christie <awchristie86@gmail.com>
2022-02-16 19:02:34 -0500

    section 20

--- components/course/index.js
+++ components/course/index.js
@@ -1,1 +1,5 @@
 export { default as List } from "./list";
+export { default as Hero } from "./hero";
+export { default as Modal } from "./modal";
+export { default as KeyPoint } from "./keypoint";
+export { default as Lecture } from "./lecture";
--- pages/index.js
+++ pages/index.js
@@ -1,25 +1,18 @@
-import { Navbar, Footer, Hero, Breadcrumbs } from "@components/common";
+import { Hero, Breadcrumbs } from "@components/common";
 import { List } from "@components/course";
+import { BaseLayout } from "@components/layout";
 import { Card } from "@components/order";
 import { WalletBar, EthRates } from "@components/web3";
 
 export default function Home() {
   return (
-    <div>
-      <div className="relative bg-white overflow-hidden">
-        <div className="relative max-w-7xl mx-auto px-4">
-          <Navbar />
-          <div className="fit">
-            <Hero />
-            <Breadcrumbs />
-            <EthRates />
-            <WalletBar />
-            <Card />
-            <List />
-          </div>
-        </div>
-        <Footer />
-      </div>
-    </div>
+    <BaseLayout>
+      <Hero />
+      <Breadcrumbs />
+      <EthRates />
+      <WalletBar />
+      <Card />
+      <List />
+    </BaseLayout>
   );
 }
