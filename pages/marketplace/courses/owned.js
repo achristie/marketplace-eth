10c423c4b8ce4074e67bb1602e8f2b714f47749a
Andrew Christie <awc@macbook-pro.lan>
2022-02-26 21:22:31 -0500

    section 30

--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -2,18 +2,24 @@ import { MarketHeader } from "@components/ui/marketplace";
 import { OwnedCourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { Button, Message } from "@components/ui/common";
+import { getAllCourses } from "@content/courses/fetcher";
+import { useAccount, useOwnedCourses } from "@components/hooks/web3";
 
-function OwnedCourses() {
+function OwnedCourses({ courses }) {
+  const { account } = useAccount();
+  const { ownedCourses } = useOwnedCourses(courses, account.data);
   return (
     <>
       <div className="py-4">
         <MarketHeader />
       </div>
       <section className="grid grid-cols-1">
-        <OwnedCourseCard>
-          <Message />
-          <Button>Watch the Course</Button>
-        </OwnedCourseCard>
+        {ownedCourses.data?.map((course) => (
+          <OwnedCourseCard key={course.id} course={course}>
+            <Message />
+            <Button>Watch the Course</Button>
+          </OwnedCourseCard>
+        ))}
       </section>
     </>
   );
@@ -24,4 +30,11 @@ const Wrapper = ({ ...props }) => (
     <OwnedCourses {...props} />
   </BaseLayout>
 );
+
+export function getStaticProps({}) {
+  const { data } = getAllCourses();
+  return {
+    props: { courses: data },
+  };
+}
 export default Wrapper;
